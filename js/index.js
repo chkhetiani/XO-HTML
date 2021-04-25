c2d2851f451cfdbdb8e1b4e2c5356d3549993f24
Irakli Chkhetiaini <chkhetiani.i@gmail.com>
2021-04-25 12:46:06 +0400

    finish

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+let to_move;
+let arr;
+let isGamePlaying;
+startGame();
 
-let to_move = 'X';
-let arr = ['', '', '', '', '', '', '', '', ''];
-setToMove();
+function startGame() {
+    to_move = 'X';
+    arr = ['', '', '', '', '', '', '', '', ''];
+    isGamePlaying = true;
+
+    document.querySelector('#result').style.display = 'none';
+    const cells = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = '';
+        cells[i].style.border = 'none';
+    }
+
+    setToMove();
+}
 
 function setToMove() {
     const el = document.querySelector('#to-move');
@@ -11,6 +27,9 @@ function setToMove() {
 
 
 function createImg() {
+    if (to_move == '') {
+        return document.createElement('div');
+    }
     const img = document.createElement('img');
     img.src = './img/' + to_move + '.png';
     img.style.width = '100%';
@@ -18,21 +37,77 @@ function createImg() {
 }
 
 function move(index) {
-    if (arr[index] === '') {
+    if (arr[index] === '' && isGamePlaying) {
         arr[index] = to_move;
 
-        const element = document.querySelector('.cell' + (index + 1));
+        const cell = document.querySelector('.cell' + (index + 1));
         const img = createImg();
-        element.appendChild(img);
+        cell.appendChild(img);
 
-        if (to_move == 'X') {
-            to_move = 'O';
-        }
-        else {
-            to_move = 'X';
-        }
+        to_move = (to_move === 'X') ? 'O' : 'X';
 
+        const result = checkWin();
+        if (result.winner != '') {
+            isGamePlaying = false;
+            to_move = '';
+            document.querySelector('.play-again').style.display = 'block';
+            drawResult(result);
+        }
         setToMove();
     }
-    console.log(index);
+}
+
+function checkWin() {
+    const winLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winLines.length; i++) {
+        const ind1 = winLines[i][0];
+        const ind2 = winLines[i][1];
+        const ind3 = winLines[i][2];
+        if (arr[ind1] === arr[ind2] && arr[ind1] === arr[ind3] && arr[ind1] != '') {
+            return {
+                winner: arr[ind1],
+                indices: winLines[i]
+            };
+        }
+    }
+    if (arr.every(function (c) { return c != '' })) {
+        return {
+            winner: 'draw'
+        }
+    }
+    return {
+        winner: ''
+    };
+}
+
+
+function drawResult(winResult) {
+    const result = document.querySelector('#result');
+    result.style.display = 'block';
+    if (winResult.winner == 'draw') {
+        result.innerText = 'The game is drawn!';
+    }
+    else {
+        result.innerText = 'The winner is ' + winResult.winner + '!';
+        for (let i = 0; i < winResult.indices.length; i++) {
+            const number = winResult.indices[i] + 1;
+            const cell = document.querySelector('.cell' + number);
+            cell.style.border = "15px solid green";
+        }
+    }
+}
+
+function startAgain() {
+    document.querySelector('.play-again').style.display = 'none';
+    startGame();
 }
